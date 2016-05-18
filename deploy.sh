8d232762516736122a8aa5d40a05dbc7f60b93e9
Charles Bergeron <chuck.bergeron@gmail.com>
2016-05-18 16:37:28 -0700

    ensure deploy script is the same on both branches

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,21 @@
 echo "Shut down the jekyll serve command before continuing ... go ahead, we'll wait ...\n"
 read -rsp $'... Press any key to continue or control-c to cancel...\n';
 
-git checkout source
+export JEKYLL_ENV=production
+
+git checkout 'master'
 bundle exec jekyll build
 git add .
 git commit -a -m "deployed new version"
-git push origin source
+git push origin 'master'
 
 rm -rf /tmp/_site
 cp -r _site /tmp/_site
-git checkout gh-pages
+git checkout 'gh-pages'
 cp -r /tmp/_site/* .
 git add .
 git commit -a -m "deployed new version"
-git push -f origin gh-pages
-git checkout source
+git push -f origin 'gh-pages'
+git checkout 'master'
 
-
+export JEKYLL_ENV=development
