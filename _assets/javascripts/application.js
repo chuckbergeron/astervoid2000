f8ae3616230b961c0808ea7899a8b39af4fceb8e
Charles Bergeron <chuck.bergeron@gmail.com>
2016-09-15 13:55:42 -0700

    Dynamacize the # of keys available and the price of the reward/game

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -15,4 +15,26 @@ $(function($) {
     game: "astervoid-2000"
   });
 
+  Itch.getGameData({
+    user: "madcapacity",
+    game: "astervoid-2000",
+    secret: "",
+    onComplete: function(data) {
+        var numKeysDiv = document.getElementById("claimed-numbers");
+        var priceDiv   = document.getElementById("buy_price");
+        var reward     = data.rewards && data.rewards[0];
+
+        if ( reward ) {
+            var total = 87 + reward.amount;
+            var amountRemaining = total - reward.amount_remaining;
+            numKeysDiv.innerText = amountRemaining + " / " + total;
+
+            priceDiv.innerText = reward.price.substr(0,2);
+        }
+        else {
+            numKeysDiv.innerText = "Rewards missing";
+        }
+    }
+  });
+
 });
